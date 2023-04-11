e48e2bb7c1e7cd19a4762e083b4644c3a0bcc73a
AKHIL-SV <svakhil007@gmail.com>
2023-04-11 20:59:35 +0530

    input and output language selection

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:language_translator/view/widget/language_selection_button.dart';
+import 'package:provider/provider.dart';
 import '../constant.dart';
+import '../controller/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,7 +14,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final TextEditingController _inputController = TextEditingController();
   final TextEditingController _outputController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -20,107 +21,119 @@ class _HomePageState extends State<HomePage> {
         body: Padding(
           padding: const EdgeInsets.all(18.0),
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Text Translation',
-                  style: TextStyle(
-                    color: white,
-                    fontSize: 18,
-                  ),
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                const Divider(
-                  color: Colors.white24,
-                ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    LanguageSelectionButton(buttonLabel: 'Translate \nFrom'),
-                    Icon(
-                      Icons.compare_arrows,
-                      color: Colors.white60,
-                    ),
-                    LanguageSelectionButton(buttonLabel: 'Translate \nTo')
-                  ],
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const [
-                    Text(
-                      'Translate From ',
+            child: Consumer<LanguageProvider>(
+              builder: (context, language, child) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Text Translation',
                       style: TextStyle(
-                        color: Colors.white38,
-                        fontWeight: FontWeight.w500,
+                        color: white,
+                        fontSize: 18,
                       ),
                     ),
-                    Text(
-                      '(Select a language)',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: white,
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    const Divider(
+                      color: Colors.white24,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        LanguageSelectionButton(
+                          buttonLabel: language.inputLanguage?.name ??
+                              'Translate \nFrom',
+                          isInputButton: true,
+                        ),
+                        const Icon(
+                          Icons.compare_arrows,
+                          color: Colors.white60,
+                        ),
+                        LanguageSelectionButton(
+                          buttonLabel:
+                              language.outputLanguage?.name ?? 'Translate \nTo',
+                          isInputButton: false,
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Text(
+                          'Translate From ',
+                          style: TextStyle(
+                            color: Colors.white38,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          '(Select a language)',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: white,
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      controller: _inputController,
+                      cursorColor: const Color(0xFFD4AF37),
+                      minLines: 6,
+                      maxLines: 8,
+                      maxLength: 2300,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        color: Colors.white60,
+                      ),
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Text(
+                          'Translate From ',
+                          style: TextStyle(
+                            color: Colors.white38,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          '(Select a language)',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: white,
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      enabled: false,
+                      controller: _outputController,
+                      minLines: 6,
+                      maxLines: 8,
+                      maxLength: 2300,
+                      cursorColor: const Color(0xFFD4AF37),
+                      style: const TextStyle(
+                        fontSize: 18,
+                        color: Colors.white60,
                       ),
                     )
                   ],
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  controller: _inputController,
-                  cursorColor: const Color(0xFFD4AF37),
-                  minLines: 6,
-                  maxLines: 8,
-                  maxLength: 2300,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    color: Colors.white60,
-                  ),
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const [
-                    Text(
-                      'Translate From ',
-                      style: TextStyle(
-                        color: Colors.white38,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    Text(
-                      '(Select a language)',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: white,
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  enabled: false,
-                  controller: _outputController,
-                  minLines: 6,
-                  maxLines: 8,
-                  maxLength: 2300,
-                  cursorColor: const Color(0xFFD4AF37),
-                  style: const TextStyle(
-                    fontSize: 18,
-                    color: Colors.white60,
-                  ),
-                )
-              ],
+                );
+              },
             ),
           ),
         ),
--- lib/view/widget/language_selection_button.dart
+++ lib/view/widget/language_selection_button.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../controller/provider.dart';
 import '../../model/language_model.dart';
 import '../../services/language_api.dart';
 import '../../constant.dart';
 
 class LanguageSelectionButton extends StatefulWidget {
-  const LanguageSelectionButton({super.key, required this.buttonLabel});
+  const LanguageSelectionButton(
+      {super.key, required this.buttonLabel, required this.isInputButton});
   final String buttonLabel;
+  final bool isInputButton;
 
   @override
   State<LanguageSelectionButton> createState() =>
@@ -32,107 +36,122 @@ class _LanguageSelectionButtonState extends State<LanguageSelectionButton> {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: kSecondaryColor,
-        minimumSize: const Size(120, 40),
-        maximumSize: const Size(120, 40),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-      ),
-      onPressed: () {
-        showBottomSheet(
-          context: context,
-          builder: (context) {
-            return Container(
-              height: 500,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Color(0xFF2C2E2F),
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(15),
-                ),
-              ),
-              padding: const EdgeInsets.all(15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'From',
-                    style: TextStyle(color: Colors.white70),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                      style: const TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        fillColor: kSecondaryColor,
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.white70),
-                            borderRadius: BorderRadius.circular(12)),
-                        enabledBorder: OutlineInputBorder(
-                            borderSide: const BorderSide(color: Colors.white30),
-                            borderRadius: BorderRadius.circular(12)),
-                        prefixIcon: const Icon(
-                          Icons.search,
-                          color: Colors.white,
-                        ),
-                      ),
-                      onChanged: (value) {}),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Text(
-                    'All Languages',
-                    style: TextStyle(color: Colors.white70),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Expanded(
-                    child: ListView.separated(
-                      itemCount: languages?.data.languages.length ?? 0,
-                      itemBuilder: (context, index) {
-                        return _isloading
-                            ? Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(12),
-                                  color: kPrimaryColor,
-                                ),
-                                child: ListTile(
-                                  title: Text(
-                                    languages!.data.languages[index].name,
-                                    style:
-                                        const TextStyle(color: Colors.white70),
-                                  ),
-                                  onTap: () {},
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                ),
-                              )
-                            : const Center(
-                                child: CircularProgressIndicator(
-                                    color: Colors.white60),
-                              );
-                      },
-                      separatorBuilder: (ctx, index) => const SizedBox(
-                        height: 15,
-                      ),
+    return Consumer<LanguageProvider>(
+      builder: (context, language, child) {
+        return ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kSecondaryColor,
+            minimumSize: const Size(120, 40),
+            maximumSize: const Size(120, 40),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          onPressed: () {
+            showBottomSheet(
+              context: context,
+              builder: (context) {
+                return Container(
+                  height: 500,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF2C2E2F),
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(15),
                     ),
-                  )
-                ],
-              ),
+                  ),
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'From',
+                        style: TextStyle(color: Colors.white70),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                          style: const TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            fillColor: kSecondaryColor,
+                            focusedBorder: OutlineInputBorder(
+                                borderSide:
+                                    const BorderSide(color: Colors.white70),
+                                borderRadius: BorderRadius.circular(12)),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    const BorderSide(color: Colors.white30),
+                                borderRadius: BorderRadius.circular(12)),
+                            prefixIcon: const Icon(
+                              Icons.search,
+                              color: Colors.white,
+                            ),
+                          ),
+                          onChanged: (value) {}),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        'All Languages',
+                        style: TextStyle(color: Colors.white70),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Expanded(
+                        child: ListView.separated(
+                          itemCount: languages?.data.languages.length ?? 0,
+                          itemBuilder: (context, index) {
+                            return _isloading
+                                ? Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(12),
+                                      color: kPrimaryColor,
+                                    ),
+                                    child: ListTile(
+                                      title: Text(
+                                        languages!.data.languages[index].name,
+                                        style: const TextStyle(
+                                            color: Colors.white70),
+                                      ),
+                                      onTap: () {
+                                        widget.isInputButton
+                                            ? language.setInputLanguage(
+                                                languages!
+                                                    .data.languages[index])
+                                            : language.setOutputLanguage(
+                                                languages!
+                                                    .data.languages[index]);
+                                        Navigator.pop(context);
+                                      },
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                    ),
+                                  )
+                                : const Center(
+                                    child: CircularProgressIndicator(
+                                        color: Colors.white60),
+                                  );
+                          },
+                          separatorBuilder: (ctx, index) => const SizedBox(
+                            height: 15,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
             );
           },
+          child: Text(
+            widget.buttonLabel,
+            textAlign: TextAlign.center,
+          ),
         );
       },
-      child: Text(
-        widget.buttonLabel,
-        textAlign: TextAlign.center,
-      ),
     );
   }
 }
