4587463f1e4f8b2262b6e92f09b77f703ab46664
AKHIL-SV <svakhil007@gmail.com>
2023-04-11 22:31:50 +0530

    language detection completed

--- lib/view/language_detection_page.dart
+++ lib/view/language_detection_page.dart
@@ -1,28 +1,106 @@
 import 'package:flutter/material.dart';
 import '../constant.dart';
+import '../services/detect_api.dart';
 
-class LanguageDetectionScreen extends StatelessWidget {
-  LanguageDetectionScreen({super.key});
+class LanguageDetectionScreen extends StatefulWidget {
+  const LanguageDetectionScreen({super.key});
+
+  @override
+  State<LanguageDetectionScreen> createState() =>
+      _LanguageDetectionScreenState();
+}
+
+String language = 'Language';
+
+class _LanguageDetectionScreenState extends State<LanguageDetectionScreen> {
   final TextEditingController _inputController = TextEditingController();
+
+  detect({required String inputText}) async {
+    final response = await DetectRequest().getDetected(text: inputText);
+    final detectedLanguage = response.data.detections[0].language;
+    setState(() {
+      language = detectedLanguage;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            TextFormField(
-                controller: _inputController,
-                cursorColor: kOrangeColor,
-                minLines: 6,
-                maxLines: 8,
-                maxLength: 2300,
-                style: const TextStyle(
-                  fontSize: 18,
-                  color: Colors.white60,
+        body: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Language Detector',
+                      style: TextStyle(
+                        color: white,
+                        fontSize: 18,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        language = 'Language';
+                      },
+                      icon: const Icon(
+                        Icons.close,
+                        color: Colors.white54,
+                      ),
+                    )
+                  ],
                 ),
-                onChanged: (value) {}),
-          ],
+                const SizedBox(
+                  height: 5,
+                ),
+                const Divider(
+                  color: Colors.white24,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.7,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        language,
+                        style: const TextStyle(
+                          fontSize: 40,
+                          color: Colors.white70,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      TextFormField(
+                        controller: _inputController,
+                        cursorColor: kOrangeColor,
+                        minLines: 6,
+                        maxLines: 8,
+                        maxLength: 2300,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.white60,
+                        ),
+                        onChanged: (value) {
+                          if (value.length >= 5) {
+                            detect(inputText: value);
+                          } else {
+                            setState(() {
+                              language = 'Type...';
+                            });
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
